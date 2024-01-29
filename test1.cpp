9a3fafcc8fb9ce7b0741de36caabce47e4c6e589
Aman Tripathi <amanttripathi02@gmail.com>
2024-01-29 14:42:00 +0530

    "this is first test commit"

---
+++ test1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ofstream fout;
+
+    string line;
+    fout.open("test.txt");
+
+    while (fout)
+    {
+        getline(cin, line);
+        if (line == "-1")
+        {
+            break;
+        }
+
+        fout << line << endl;
+    }
+
+    fout.close();
+    ifstream fin;
+
+    fin.open("test.txt");
+    while (getline(fin, line))
+    {
+        cout << line << endl;
+    }
+    fin.close();
+    return 0;
+}
