ae1aecd7f5af0f83949882815ac5f247dc97df3c
Aman Tripathi <amanttripathi02@gmail.com>
2026-03-07 04:54:38 +0530

    created a decorator function

---
+++ ml_code/dm lab -3/decorator_1.py
@@ -0,0 +1,16 @@
+from functools import wraps
+
+def log_activity(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Executing {func.__name__} with args: {args} and kwargs: {kwargs}")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} returned: {result}")
+        return result
+    return wrapper
+
+@log_activity
+def add(a, b):
+    return a + b
+
+add(5, 3)
